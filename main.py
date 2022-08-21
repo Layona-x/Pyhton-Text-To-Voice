7e47c29b76b22501544a34209f207d96cc007516
Layona-x <109739779+Layona-x@users.noreply.github.com>
2022-08-21 19:37:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from gtts import gTTS 
 import os
-import time
 
 d = open("konus.txt","r")
 text_d = d.read().replace("/n"," ")
 tts = gTTS(text=text_d, lang='tr')
-#Burada oluşturduğumuz ses dosyasını konuma merhaba.mp3 diye kaydediyoruz
 tts.save("hay.mp3")
 d.close()
-#şimdi ise bu dosyayı açalım.
 os.system("start hay.mp3")
